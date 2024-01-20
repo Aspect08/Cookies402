7041471822a3b4b8f12df56c31d510219dc96f8e
Cookies402 <Cookies402@proton.me>
2024-01-21 02:30:53 +0530

    NICE COMMIT MSG

--- scr/mail.js
+++ scr/mail.js
@@ -1,25 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDFDBFWWWLTpD6kqaOrf3i49xB9t-bxLrI",
-  authDomain: "pawsguard-79aa0.firebaseapp.com",
-  databaseURL: "https://pawsguard-79aa0-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pawsguard-79aa0",
-  storageBucket: "pawsguard-79aa0.appspot.com",
-  messagingSenderId: "778272185845",
-  appId: "1:778272185845:web:1a90ab3c72d7fad1ee54bc",
-  measurementId: "G-E3ES908TZF"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const storageRef = ref(storage);
-const { ArtifactRegistryClient } = require('@google-cloud/artifact-registry');
+// ... (previous code)
 
 async function listRepositoriesAndPackages() {
   // Set your Google Cloud project ID
-  const projectId = 'your-project-id';
+  const projectId = 'pawsguard-79aa0'; // Correct project ID
   const location = 'us-central1'; // Set your desired location
 
   const client = new ArtifactRegistryClient();
@@ -35,7 +18,7 @@ async function listRepositoriesAndPackages() {
     });
 
     // Example: List packages in a repository
-    const repositoryName = 'your-repository';
+    const repositoryName = 'Codefest_2024'; // Replace with your actual repository name
     const packages = await client.listPackages({
       parent: `projects/${projectId}/locations/${location}/repositories/${repositoryName}`,
     });
@@ -51,5 +34,3 @@ async function listRepositoriesAndPackages() {
 
 // Run the function
 listRepositoriesAndPackages();
-
-
